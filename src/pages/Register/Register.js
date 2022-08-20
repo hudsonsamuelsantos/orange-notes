afb3bf5a5dc4a829890225dc8db1f489f8ba40bb
Hudson Santos <hudsnss@gmail.com>
2022-08-20 14:24:37 -0300

    register page update

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -2,7 +2,49 @@ import styles from './Register.module.css'
 
 function Register() {
     return (
-        <div>Register</div>
+        <div className={styles.register}>
+            <h1>Cadastre-se para começar</h1>
+            <p>Crie sua conta e começe a organizar suas anotações</p>
+            <form>
+                <label>
+                    <span>Nome:</span>
+                    <input
+                        type="text"
+                        name='displayName'
+                        required
+                        placeholder='Insira seu nome de usuário'
+                    />
+                </label>
+                <label>
+                    <span>E-mail:</span>
+                    <input
+                        type='email'
+                        name='email'
+                        required
+                        placeholder='Insira seu e-mail'
+                    />
+                </label>
+                <label>
+                    <span>Senha:</span>
+                    <input
+                        type='password'
+                        name='password'
+                        required
+                        placeholder='Insira sua senha'
+                    />
+                </label>
+                <label>
+                    <span>Confirmar senha:</span>
+                    <input
+                        type='password'
+                        name='confirmPassword'
+                        required
+                        placeholder='Insira novamente sua senha'
+                    />
+                </label>
+                <button className='btn'>Cadastrar</button>
+            </form>
+        </div>
     )
 }
 
