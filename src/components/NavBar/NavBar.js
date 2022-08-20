0f87dfe437b0b273be206e9cc25ede7157a1bf9b
Hudson Santos <hudsnss@gmail.com>
2022-08-20 15:44:30 -0300

    user in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,36 @@ import styles from './NavBar.module.css'
 
 import { NavLink } from 'react-router-dom'
 
+import { useAuthentication } from '../../hooks/useAuthentication'
+
+import { useAuthValue } from '../../context/AuthContext'
+
 export function NavBar() {
+
+    const { user } = useAuthValue()
+
     return (
-        <nav>
-            <NavLink to='/'>
-                <span>Orange</span> Notes
+        <nav className={styles.nav_bar}>
+            <NavLink to='/' className={styles.brand}>
+                <span>Orange</span>Notes
             </NavLink>
+
+            {user &&
+                <>
+                    <ul className={styles.links_list}>
+                        <li>
+                            <NavLink to='/dashboard' className={({ isActive }) => (isActive ? styles.active : '')}>Dashboard</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : '')}>Sair</NavLink>
+                        </li>
+                    </ul>
+                    <div>Logado como:
+                        <span>{user.displayName}</span>
+                    </div>
+                </>
+            }
+
         </nav>
     )
 }
