16ac9aa8e3f44878f91322033575f9de9bc60533
Hudson Santos <hudsnss@gmail.com>
2022-08-22 20:34:19 -0300

    fetch hook

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import { db } from '../firebase/config'
+import {
+    collection,
+    query,
+    orderBy,
+    onSnapshot,
+    where,
+    QuerySnapshot,
+} from 'firebase/firestore'
+
+export const useFetchDocuments = (docCollection, search = null) => {
+
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    //memory leak
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(() => {
+
+        async function loadData() {
+            if (cancelled) return
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+
+            try {
+
+                let q
+                q = await query(collectionRef, orderBy('createdAt', 'desc'))
+
+                await onSnapshot(q, (QuerySnapshot) => {
+
+                    setDocuments(
+                        QuerySnapshot.docs.map(doc => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+
+                })
+
+                setLoading(false)
+
+            } catch (error) {
+                console.log(error)
+                setError(error.message)
+
+                setLoading(false)
+            }
+
+        }
+
+        loadData()
+
+        useEffect(() => {
+            return () => setCancelled(true)
+        }, [])
+
+    }, [docCollection, search, cancelled])
+
+} 
