2d5569cfb0d4096b2f166cdd8aee173d8b3ffafe
Hudson Santos <hudsnss@gmail.com>
2022-08-23 20:20:30 -0300

    fetch hook update (search)

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -30,7 +30,16 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
             try {
 
                 let q
-                q = await query(collectionRef, orderBy('createdAt', 'desc'))
+
+                if (search) {
+                    q = await query(
+                        collectionRef,
+                        where('tags', 'array-contains', search),
+                        orderBy('createdAt', 'desc')
+                    )
+                } else {
+                    q = await query(collectionRef, orderBy('createdAt', 'desc'))
+                }
 
                 await onSnapshot(q, (QuerySnapshot) => {
 
