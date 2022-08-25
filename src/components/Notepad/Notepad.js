c06b67bc439c09e5710737d90658982ca667ca87
Hudson Santos <hudsnss@gmail.com>
2022-08-25 19:29:58 -0300

    delete block functionality

--- src/components/Notepad/Notepad.js
+++ src/components/Notepad/Notepad.js
@@ -1,7 +1,21 @@
-import { Link } from 'react-router-dom'
 import styles from './Notepad.module.css'
 
+import { useState } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import { useDeleteDocument } from '../../hooks/useDeleteDocument'
+
 function Notepad({ notepad }) {
+
+    const [selectedNoteIndex, setSelectedNoteIndex] = useState()
+
+    const { deleteDocument } = useDeleteDocument('notepads')
+
+    const notepadId = notepad.id
+
+    console.log(selectedNoteIndex)
+
     return (
         <div>
             <table>
@@ -13,14 +27,22 @@ function Notepad({ notepad }) {
                                 + Adicionar anotação
                             </Link>
                         </th>
+                        <th>
+                            <button onClick={() => deleteDocument(notepadId)}>Excluir Bloco</button>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     {notepad.notes &&
-                        notepad.notes.map(note => (
+                        notepad.notes.map((note, index) => (
                             <tr key={note}>
                                 <td>
                                     {note}
+                                </td>
+                                <td>
+                                    <button onClick={() => setSelectedNoteIndex(index)}>
+                                        Excluir
+                                    </button>
                                 </td>
                             </tr>
                         ))
