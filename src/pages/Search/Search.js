1037cd47415cfd5ab86641097fb9c8d2128510a9
Hudson Santos <hudsnss@gmail.com>
2022-08-30 21:54:05 -0300

    search page css resposiveness

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -8,6 +8,7 @@ import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useNavigate, Link } from 'react-router-dom'
 
 import { useDeleteDocument } from '../../hooks/useDeleteDocument'
+import { ArrowCircleLeft, Eye, Notepad, NotePencil, Trash } from 'phosphor-react'
 
 function Search() {
 
@@ -17,42 +18,75 @@ function Search() {
     const query = useQuery()
     const search = query.get('q')
 
-    const { documents: notepads } = useFetchDocuments('notepads', search, uid)
+    const { documents: notepads, loading } = useFetchDocuments('notepads', search, uid)
 
     const { deleteDocument } = useDeleteDocument('notepads')
 
     return (
         <div>
-            <Link to={'/dashboard'}>- Voltar</Link>
+            <Link to={`/dashboard`} className={styles.back_to_dashboard}>
+                <ArrowCircleLeft size={32} />
+                <span>Voltar</span>
+            </Link>
             <h1>Resultados</h1>
-            <p>Voce buscou por: #{search}</p>
+
             <div>
-                {notepads &&
-                    notepads.map(notepad => (
-                        <div key={notepad.id}>
-                            <div>
-                                <span>{notepad.notepadTitle}</span>
-                                <div>
-                                    <span>Tags:</span>
-                                    <span>
-                                        {notepad.tagsArray &&
-                                            notepad.tagsArray.map((tag, index) => (
-                                                <span key={index}>
-                                                    {`#${tag}   `}
-                                                </span>
-                                            ))
-                                        }
-                                    </span>
-                                </div>
+                <p>Voce buscou por: <strong>#{search}</strong></p>
+                <div className={styles.dashboard_table}>
+                    {loading && <p>Carregando...</p>}
+                    <div>
+                        <div className={styles.dashboard_table_header}>
+                            <div className={styles.dashboard_table_header_title}>
+                                <Notepad size={32} />
+                                <span>Blocos de Notas Encontrados</span>
                             </div>
-                            <Link to={`/notes/${notepad.id}`}>- Ver anotações</Link>
-                            <Link to={'/'} onClick={() => deleteDocument(notepad.id)}>- Excluir bloco</Link>
+
                         </div>
-                    ))
-                }
-                {notepads && notepads.length === 0 && (
-                    <span>Não encontramos nenhum bloco de notas com essa tag...</span>
-                )}
+
+                        <div>
+                            {notepads &&
+                                notepads.map(notepad => (
+                                    <div key={notepad.id} className={styles.dashboard_table_notepad}>
+                                        <div className={styles.notepad}>
+                                            <span className={styles.notepad_title}>{notepad.notepadTitle}</span>
+                                            <div className={styles.notepad_tags}>
+                                                <span>Tags:</span>
+                                                <span>
+                                                    {notepad.tagsArray &&
+                                                        notepad.tagsArray.map((tag, index) => (
+                                                            <span key={index}>
+                                                                {`#${tag}      `}
+                                                            </span>
+                                                        ))
+                                                    }
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className={styles.actions}>
+                                            <Link to={`/notes/${notepad.id}`} className={styles.see}>
+                                                <Eye size={24} color={'blueviolet'} />
+                                                <span>Ver Anotações</span>
+                                            </Link>
+                                            <Link to={`/notepads/${notepad.id}`} className={styles.edit}>
+                                                <NotePencil size={24} color={'blue'} />
+                                                <span>Editar</span>
+                                            </Link>
+                                            <Link to={'/'} onClick={() => deleteDocument(notepad.id)} className={styles.delete}>
+                                                <Trash size={24} color={'red'} />
+                                                <span>Excluir</span>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                            {notepads && notepads.length === 0 && (
+                                <div className={styles.message}>
+                                    <span>Nenhum bloco de notas foi encontrado...</span>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div >
             </div>
         </div>
     )
