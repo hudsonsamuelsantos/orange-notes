708d7841a5697965450f89ffedb8ef6d03203b7f
Hudson Santos <hudsnss@gmail.com>
2022-09-01 06:09:28 -0300

    register page css responsiveness

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from 'react'
 
 import { useAuthentication } from '../../hooks/useAuthentication'
 
+import { ArrowCircleLeft } from 'phosphor-react'
+
+import { Link } from 'react-router-dom'
+
 function Register() {
 
     const [displayName, setDisplayName] = useState('')
@@ -45,57 +49,65 @@ function Register() {
 
     return (
         <div className={styles.register}>
-            <h1>Cadastre-se para começar</h1>
-            <p>Crie sua conta e começe a organizar suas anotações</p>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Nome:</span>
-                    <input
-                        type="text"
-                        name='displayName'
-                        required
-                        placeholder='Insira seu nome de usuário'
-                        value={displayName}
-                        onChange={e => setDisplayName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>E-mail:</span>
-                    <input
-                        type='email'
-                        name='email'
-                        required
-                        placeholder='Insira seu e-mail'
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>Senha:</span>
-                    <input
-                        type='password'
-                        name='password'
-                        required
-                        placeholder='Insira sua senha'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </label>
-                <label>
-                    <span>Confirmar senha:</span>
-                    <input
-                        type='password'
-                        name='confirmPassword'
-                        required
-                        placeholder='Insira novamente sua senha'
-                        value={confirmPassword}
-                        onChange={e => setConfirmPassword(e.target.value)}
-                    />
-                </label>
-                {!loading && <button className='btn' onClick={handleSubmit}>Cadastrar</button>}
-                {loading && <button className='btn' onClick={handleSubmit} disabled>Aguarde...</button>}
-                {error && <p className='error'>{error}</p>}
-            </form>
+            <div className={styles.back_to_login_box}>
+                <Link to={`/dashboard`} className={styles.back_to_login}>
+                    <ArrowCircleLeft size={32} />
+                    <span>Voltar</span>
+                </Link>
+            </div>
+            <h1>Crie sua conta</h1>
+            <p>Comece agora a progredir em organizar seu conhecimento!</p>
+            <div className={styles.register_display}>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <span>Nome:</span>
+                        <input
+                            type="text"
+                            name='displayName'
+                            required
+                            placeholder='Insira seu nome de usuário'
+                            value={displayName}
+                            onChange={e => setDisplayName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        <span>E-mail:</span>
+                        <input
+                            type='email'
+                            name='email'
+                            required
+                            placeholder='Insira seu e-mail'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        <span>Senha:</span>
+                        <input
+                            type='password'
+                            name='password'
+                            required
+                            placeholder='Insira sua senha'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        <span>Confirmar senha:</span>
+                        <input
+                            type='password'
+                            name='confirmPassword'
+                            required
+                            placeholder='Insira novamente sua senha'
+                            value={confirmPassword}
+                            onChange={e => setConfirmPassword(e.target.value)}
+                        />
+                    </label>
+                    {!loading && <button className={styles.btn} onClick={handleSubmit}>Cadastrar</button>}
+                    {loading && <button className={styles.btn} onClick={handleSubmit} disabled>Aguarde...</button>}
+                    {error && <p className='error'>{error}</p>}
+                </form>
+            </div>
         </div>
     )
 }
